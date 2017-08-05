005c7e1236435491bed21c8b728bb3ef5173e81b
Timothy Alexander <dragonfyre13@gmail.com>
2017-08-05 00:20:38 -0500

    nulled out log files in tar, renamed tar.gz file to proper extension for it's file type and pre-changed browser launch settings in jdownloader (instead of requiring the user change the config post-install)

--- firstrun.sh
+++ firstrun.sh
@@ -3,7 +3,7 @@
   mkdir -p /config/Downloads
   
   if [ ! -d /config/jd2 ]; then
-    tar -zxvf /nobody/jd2.tar -C /config/
+    tar -zxvf /nobody/jd2.tar.gz -C /config/
   fi
   chown -R nobody:users /config
   chmod -R g+rw /config
