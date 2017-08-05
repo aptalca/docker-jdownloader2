f69d313077a08426eec9817ac00159d01ac6380c
Timothy Alexander <dragonfyre13@gmail.com>
2017-08-05 00:38:49 -0500

    Fixing an issue with a missing tomcat7 cache directory

--- firstrun.sh
+++ firstrun.sh
@@ -3,5 +3,9 @@
   if [ ! -d /config/jd2 ]; then
     tar -zxvf /nobody/jd2.tar.gz -C /config/
   fi
+  if [ ! -d /var/cache/tomcat7 ]; then
+    mkdir -p /var/cache/tomcat7
+    chown tomcat7:tomcat7 /var/cache/tomcat7
+  fi
   chown -R nobody:users /config
   chmod -R g+rw /config
